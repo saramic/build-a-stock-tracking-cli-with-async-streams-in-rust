d6fb46c56085ee6740ef7315e4e7ebac846a6e46
Michael Milewski <saramic@gmail.com>
2021-10-20 10:47:14 +1100

    3.2.3 serilialize some JSON output using serde
    using derive and serde for serlializing as well as implementing Question
    and QuestionId as well as implementation of new and from_str, we can now
    serialize some output
      Ok(warp::reply::json(&question))
    to get the following on the endpoint
      http://127.0.0.1:1337/questions
      {
        id: "1",
        title: "First Question",
        content: "Content of question",
        tags: [
          "faq"
        ]
      }

--- rust-web-development/ch-3-route-handling/src/main.rs
+++ rust-web-development/ch-3-route-handling/src/main.rs
@@ -1,9 +1,62 @@
+use serde::Serialize;
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
 use warp::Filter;
+
+#[derive(Debug, Serialize)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+#[derive(Debug, Serialize)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+    Ok(warp::reply::json(&question))
+}
 
 #[tokio::main]
 async fn main() {
-    let hi = warp::path("hello").map(|| format!("Hello, World!"));
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
 
-    println!("listening on http://127.0.0.1:1337\nbut only serving http://127.0.0.1:1337/hello");
-    warp::serve(hi).run(([127, 0, 0, 1], 1337)).await
+    let routes = get_items;
+
+    println!(
+        "listening on http://127.0.0.1:1337\nbut only serving http://127.0.0.1:1337/questions"
+    );
+
+    warp::serve(routes).run(([127, 0, 0, 1], 1337)).await
 }
