c0c0044be91d637929680de6828a9dedb4989810
Michael Milewski <saramic@gmail.com>
2021-10-20 11:00:28 +1100

    recover from errors
    NOW
      curl --location --request GET 'localhost:1337/questions'
      {"id":"1","title":"First Question","content":"Content of question","tags":["faq"]}
    AND
      curl --location --request GET 'localhost:1337/questions/2'
      Route not found

--- rust-web-development/ch-3-route-handling/src/main.rs
+++ rust-web-development/ch-3-route-handling/src/main.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
-use warp::Filter;
+use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -35,6 +35,10 @@ impl FromStr for QuestionId {
     }
 }
 
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
 async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
@@ -42,7 +46,25 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
         "Content of question".to_string(),
         Some(vec!["faq".to_string()]),
     );
-    Ok(warp::reply::json(&question))
+
+    match question.id.0.is_empty() {
+        true => Err(warp::reject::custom(InvalidId)),
+        false => Ok(warp::reply::json(&question)),
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
 }
 
 #[tokio::main]
@@ -50,7 +72,8 @@ async fn main() {
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
-        .and_then(get_questions);
+        .and_then(get_questions)
+        .recover(return_error);
 
     let routes = get_items;
 
